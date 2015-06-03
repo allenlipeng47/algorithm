1b287dfb97d5ce0494839e62157ac64395bdfb36
allenlipeng47 <allen.lipeng47@gmail.com>
2015-06-02 21:17:21 -0700

    change to addFirst(), addLast(), removeFirst(), removeLast() functions

--- src/main/java/com/pli/project/algorithm/hash/rollinghash/RollingHashQueue.java
+++ src/main/java/com/pli/project/algorithm/hash/rollinghash/RollingHashQueue.java
@@ -1,5 +1,8 @@
 package com.pli.project.algorithm.hash.rollinghash;
 
+import lombok.Getter;
+import org.junit.Test;
+
 import java.math.BigInteger;
 import java.util.LinkedList;
 
@@ -8,38 +11,62 @@ import java.util.LinkedList;
  */
 public class RollingHashQueue<E> {
 
-    // base is the number of characters in input alphabet
+    /** base is the number of characters in input alphabet **/
     private final int base = 31;
 
-    // q is a prime number used for evaluating Rabin Karp's Rolling hash
+    /** q is a prime number used for evaluating Rabin Karp's Rolling hash **/
     private final int q = 103;
 
-    //magic base^(n-1). n is the number of element the list has.
+    /** magic base^(n-1). n is the number of element the list has. **/
     private int magic = 1;
 
     LinkedList<E> list;
 
+    @Getter
     private int hash;
+
+    private int magicModInverse;
 
     RollingHashQueue(){
         list = new LinkedList<E>();
+        magicModInverse = BigInteger.valueOf(base).modInverse(BigInteger.valueOf(q)).intValue();
     }
 
-    public boolean enQueue(E e){
+    /** enQueue, add element to queue tail **/
+    public boolean addLast(E e){
         list.addLast(e);
         hash = ((hash * base) % q + e.hashCode()) % q;
         magic = (magic * base) % q;
         return true;
     }
 
-    public E deQueue(){
+    /** add element to queue head **/
+    public boolean addFirst(E e){
+        list.addFirst(e);
+        hash = ((magic * e.hashCode()) % q + hash ) % q;
+        magic = (magic * base) % q;
+        return true;
+    }
+
+    /** deQueue, remove element from queue head **/
+    public E removeFirst(){
         if(list==null || list.size()==0){
             return null;
         }
-        int magicModInverse = BigInteger.valueOf(base).modInverse(BigInteger.valueOf(q)).intValue();
         magic = (magic * magicModInverse) % q;
         hash = (hash - (magic * list.get(0).hashCode()) % q + q) % q;
         E e = list.remove(0);
+        return e;
+    }
+
+    /** remove element from queue tail **/
+    public E removeLast(){
+        if(list==null || list.size()==0){
+            return null;
+        }
+        magic = (magic * magicModInverse) % q;
+        hash = (((hash - list.getLast().hashCode() + q) % q) * magicModInverse) % q;
+        E e = list.removeLast();
         return e;
     }
 
@@ -48,31 +75,7 @@ public class RollingHashQueue<E> {
     }
 
     public static void main(String[] args) {
-        RollingHashQueue<Character> queue = new RollingHashQueue<Character>();
-        queue.enQueue('a');        System.out.println(queue.hash);
-        queue.enQueue('b');        System.out.println(queue.hash);
-        queue.enQueue('a');        System.out.println(queue.hash);
-        queue.enQueue('d');        System.out.println(queue.hash);
-        queue.deQueue();        System.out.println(queue.hash);
-        queue.enQueue('a');        System.out.println(queue.hash);
-        queue.enQueue('b');        System.out.println(queue.hash);
-        queue.deQueue();        System.out.println(queue.hash);
-        queue.enQueue('a');        System.out.println(queue.hash);
-        queue.deQueue();        System.out.println(queue.hash);
-        queue.deQueue();        System.out.println(queue.hash);
-        LinkedList<Character> list = new LinkedList<Character>();
-        list.add('a');        System.out.println(list.hashCode());
-        list.add('b');        System.out.println(list.hashCode());
-        list.add('a');        System.out.println(list.hashCode());
-        list.remove(0);        System.out.println(list.hashCode());
-        list.add('d');        System.out.println(list.hashCode());
-        list.remove(0);        System.out.println(list.hashCode());
-        list.add('a');        System.out.println(list.hashCode());
-        list.remove(0);        System.out.println(list.hashCode());
-        list.add('b');        System.out.println(list.hashCode());
-        list.add('a');        System.out.println(list.hashCode());
-        list.remove(0);        System.out.println(list.hashCode());
-        System.out.println();
+        System.out.println("a");
     }
 
 }
