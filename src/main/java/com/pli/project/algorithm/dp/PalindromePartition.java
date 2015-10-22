3af0014aeb131bbc6adaad37b6abbf432916572a
allenlipeng47 <allen.lipeng47@gmail.com>
2015-10-21 23:17:05 -0700

    Added minimum palindrome partition.

---
+++ src/main/java/com/pli/project/algorithm/dp/PalindromePartition.java
@@ -0,0 +1,36 @@
+package com.pli.project.algorithm.dp;
+
+/**
+ * Created by lipeng on 2015/10/21.
+ * Given a string, we try to split the string to each sub-string. Each sub-string needs to be palindrome.
+ */
+public class PalindromePartition {
+
+    public static int getMinPalindromePartition(char[] chs) {
+        int[][] arr = new int[chs.length][chs.length];
+        for(int i=0; i<chs.length; i++) {
+            arr[i][i] = 1;
+        }
+        for(int gap=1; gap<chs.length; gap++) {
+            for(int i=0; i<chs.length-gap; i++) {
+                if(arr[i+1][i+gap-1]<=1&&chs[i]==chs[i+gap]) {
+                    arr[i][i+gap] = 1;
+                }
+                else {
+                    int minPartition = Integer.MAX_VALUE;
+                    for(int k=0; k<gap; k++) {
+                        minPartition = Math.min(minPartition, arr[i][i+k]+arr[i+k+1][i+gap]);
+                    }
+                    arr[i][i+gap] = minPartition;
+                }
+            }
+        }
+        return arr[0][chs.length-1];
+    }
+
+    public static void main(String[] args) {
+        String str = "abacbdbc";
+        System.out.println(getMinPalindromePartition(str.toCharArray()));
+    }
+
+}
