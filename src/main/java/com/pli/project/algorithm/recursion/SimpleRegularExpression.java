ebde5012354fe5f6f5887b1a7371e225374ee0c7
allenlipeng47 <allen.lipeng47@gmail.com>
2015-12-17 23:49:42 -0800

    Made code simplifier.

--- src/main/java/com/pli/project/algorithm/recursion/SimpleRegularExpression.java
+++ src/main/java/com/pli/project/algorithm/recursion/SimpleRegularExpression.java
@@ -6,8 +6,7 @@ package com.pli.project.algorithm.recursion;
 public class SimpleRegularExpression {
 
     public static boolean isMatch(String str, String reg) {
-        if(str==null || reg==null)
-            return false;
+        assert str!=null && reg!=null;
         return matchHelper(str, reg, 0, 0);
     }
 
@@ -16,23 +15,18 @@ public class SimpleRegularExpression {
             return true;
         if((i>=str.length() && str.length()>0) || j>=reg.length())
             return false;
-        if(j<reg.length()-1 && reg.charAt(j + 1)=='*') {
-            // because char after j is *, so we should decide where to skip * in reg
+        if(j<reg.length() -1 && reg.charAt(j + 1)=='*') // because char after j is *, so we should decide where to skip * in reg
             if (matchHelper(str, reg, i, j + 2)) {  // consider * is 0 times
                 return true;
-            } else {    // consider * happens from 1, 2 and more times
+            } else    // consider * happens from 1, 2 and more times
                 for (; i < str.length() && (str.charAt(i) == reg.charAt(j) || reg.charAt(j) == '.'); i++)
                     if (matchHelper(str, reg, i + 1, j + 2))
                         return true;
-            }
-        }
-        if((str.charAt(i)==reg.charAt(j) || reg.charAt(j)=='.') && matchHelper(str, reg, i+1, j+1))
-            return true;
-        return false;
+        return (str.charAt(i)==reg.charAt(j) || reg.charAt(j)=='.') && matchHelper(str, reg, i+1, j+1);
     }
 
     public static void main(String[] args) {
-        System.out.println(isMatch("", ".*"));
+        System.out.println(isMatch("ab", "c*abd"));
     }
 
 }
