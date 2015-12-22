c684ecf0642399700a4905b4ca5533650dfb5baf
allenlipeng47 <allen.lipeng47@gmail.com>
2015-12-22 11:28:43 -0800

    Added H2O thread.
    There are multiple factories producing H, multiple factories producing O. Once there is at least 2 H, O 1, then populate a H2O.

---
+++ src/main/java/com/pli/project/algorithm/thread/H2O.java
@@ -0,0 +1,71 @@
+package com.pli.project.algorithm.thread;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by pli on 12/22/2015.
+ */
+public class H2O {
+
+    private int h;
+
+    private int o;
+
+    Semaphore mutex = new Semaphore(1);
+
+    private void produceH2O() {
+        System.out.println(Thread.currentThread().getId() + " enter h:" + h + ", o:" + o);
+        if(h>=2 && o>=1) {
+            h = h-2;
+            o = o-1;
+            System.out.println("H2O");
+        }
+        System.out.println(Thread.currentThread().getId() + " leave status h:" + h + ", o:" + o);
+    }
+
+    class WaterFactoryH implements Runnable {
+        public void run() {
+            while (true) {
+                try {
+                    mutex.acquire();
+                    h++;
+                    produceH2O();
+                    mutex.release();
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    System.out.println("thread is interrupted");
+                }
+            }
+        }
+    }
+
+    class WaterFactoryO implements Runnable {
+        public void run() {
+            while (true) {
+                try {
+                    mutex.acquire();
+                    o++;
+                    produceH2O();
+                    mutex.release();
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    System.out.println("thread is interrupted");
+                }
+            }
+        }
+    }
+
+    public void run() {
+        for(int i=0; i<2; i++)
+            new Thread(new WaterFactoryH()).start();
+        for(int i=0; i<2; i++)
+            new Thread(new WaterFactoryO()).start();
+    }
+
+
+    public static void main(String[] args) {
+        H2O h2o = new H2O();
+        h2o.run();
+    }
+
+}
