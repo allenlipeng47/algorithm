e0924367edbbae6616795914d1e27abdadd4d071
allenlipeng47 <allen.lipeng47@gmail.com>
2016-01-02 19:11:43 -0800

    https://leetcode.com/problems/gray-code/ http://mathworld.wolfram.com/GrayCode.html

---
+++ src/main/java/com/pli/project/algorithm/sort/GrayCode.java
@@ -0,0 +1,25 @@
+package com.pli.project.algorithm.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lipeng on 2016/1/2.
+ */
+public class GrayCode {
+
+    public static List<Integer> grayCode(int n) {
+        List<Integer> ans = new ArrayList<Integer>();
+        ans.add(0);
+        for(int i = 0, base = 1; i < n; i++, base = base << 1) {
+            for (int j = ans.size() - 1; j >= 0; j--)
+                ans.add(ans.get(j) | base);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> ans = grayCode(3);
+        System.out.println();
+    }
+}
