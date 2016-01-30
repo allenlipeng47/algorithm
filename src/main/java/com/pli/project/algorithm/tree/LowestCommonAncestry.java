51124b57f513f93d4962324aefca12eae151a5e3
allenlipeng47 <allen.lipeng47@gmail.com>
2016-01-29 23:57:46 -0800

    This is an awesome code for finding lca.

---
+++ src/main/java/com/pli/project/algorithm/tree/LowestCommonAncestry.java
@@ -0,0 +1,34 @@
+package com.pli.project.algorithm.tree;
+
+/**
+ * Created by lipeng on 2016/1/29
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/.
+ */
+public class LowestCommonAncestry {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q || root == null) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left == null && right == null) {
+            return null;
+        }
+        if (left != null) {
+            return left;
+        }
+        return right;
+    }
+
+}
