3aa4ceb03a76767b05b58aa7194f15d4fb65db1d
allenlipeng47 <allen.lipeng47@gmail.com>
2016-04-10 15:40:15 -0700

    https://leetcode.com/problems/number-of-islands/

---
+++ src/main/java/com/pli/project/algorithm/leetcode/NumOfIslands.java
@@ -0,0 +1,40 @@
+package com.pli.project.algorithm.leetcode;
+
+import com.sun.media.sound.SoftLanczosResampler;
+
+/**
+ * Created by lipeng on 2016/4/10.
+ */
+public class NumOfIslands {
+
+    public int numIslands(char[][] grid) {
+        int ans = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    ans++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return ans;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+
+    public static void main(String[] args) {
+        NumOfIslands n = new NumOfIslands();
+        char[][] chs = {{'0'}};
+        System.out.println(n.numIslands(chs));
+
+    }
+}
