a13326488c13969d8dede8eece2943092f2622d5
allenlipeng47 <allen.lipeng47@gmail.com>
2016-04-18 23:05:38 -0700

    https://leetcode.com/problems/power-of-four/ First of all, any negative number should return false. 2nd, we can use num & (-num) to test if binary form only has one 1. In another word, to see if num is power of 2. In the end, because power of four only has 1 in odd position:             1         100     10000 1000000 To test if 1 exists at odd position, we need to use num & 0x55555555

---
+++ src/main/java/com/pli/project/algorithm/leetcode/PowerOf4.java
@@ -0,0 +1,16 @@
+package com.pli.project.algorithm.leetcode;
+
+/**
+ * Created by lipeng on 2016/4/18.
+ */
+public class PowerOf4 {
+
+    public static boolean isPowerOfFour(int num) {
+        return (num & (num - 1)) == 0 && (num & (-num) & 0x55555555) != 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(1));
+
+    }
+}
