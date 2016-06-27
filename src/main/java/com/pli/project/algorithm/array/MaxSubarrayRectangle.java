bf4f038023bac6f9786eff56349ca614e88f4330
allenlipeng47 <allen.lipeng47@gmail.com>
2016-06-27 00:10:11 -0700

    Maximum sum rectangle in a 2D matrix. This ia a further problem from MaxSubarray.

---
+++ src/main/java/com/pli/project/algorithm/array/MaxSubarrayRectangle.java
@@ -0,0 +1,41 @@
+package com.pli.project.algorithm.array;
+
+/**
+ * Created by lipeng on 2016/6/26.
+ * http://www.geeksforgeeks.org/dynamic-programming-set-27-max-sum-rectangle-in-a-2d-matrix/
+ */
+public class MaxSubarrayRectangle {
+
+    public static int maxSubArrayRectangle(int [][]arr) {
+        int height = arr.length, width = arr[0].length, max = arr[0][0];
+        int[] helperArr = null;
+        for (int left = 0; left < width; left++) {
+            helperArr = new int[width];
+            for (int right = left; right < width; right++) {
+                // update helper
+                for (int i = 0; i < height; i++) {
+                    helperArr[i] += arr[i][right];
+                }
+                // Kodane algorithm to find the top and bottom for max continuous sequence
+                int helperMax = helperArr[0], currHelperMax = helperArr[0];
+                for (int i = 0; i < height; i++) {
+                    currHelperMax = Math.max(currHelperMax + helperArr[i], helperArr[i]);
+                    helperMax = Math.max(helperMax, currHelperMax);
+                }
+                max = Math.max(currHelperMax, max);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, -1, -4, -20},
+                {-8, -3, 4, 2, 1},
+                {3, 8, 10, 1, 3},
+                {-4, -1, 1, 7, -6}
+        };
+        System.out.println(maxSubArrayRectangle(arr));
+    }
+
+}
