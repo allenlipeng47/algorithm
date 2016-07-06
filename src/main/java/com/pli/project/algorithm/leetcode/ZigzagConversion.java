d113f89bea2707da758809b74b196868658e5c5b
allenlipeng47 <allen.lipeng47@gmail.com>
2016-07-06 07:11:20 -0700

    Improved the result return part.

--- src/main/java/com/pli/project/algorithm/leetcode/ZigzagConversion.java
+++ src/main/java/com/pli/project/algorithm/leetcode/ZigzagConversion.java
@@ -13,18 +13,17 @@ public class ZigzagConversion {
         }
         int i = 0, len = s.length();
         while (i < len) {
-            for (int idx = 0; idx < numRows && i < len; idx++, i++) {
+            for (int idx = 0; idx < numRows && i < len; idx++, i++) {   // build vertical part
                 sbs[idx].append(s.charAt(i));
             }
-            for (int idx = numRows - 2; idx >= 1 && i < len; idx--, i++) {
+            for (int idx = numRows - 2; idx >= 1 && i < len; idx--, i++) {  // build oblique part
                 sbs[idx].append(s.charAt(i));
             }
         }
-        StringBuffer ans = new StringBuffer();
-        for (StringBuffer sb : sbs) {
-            ans.append(sb);
+        for (int j = 1; j < numRows; j++) {
+            sbs[0].append(sbs[j]);
         }
-        return ans.toString();
+        return sbs[0].toString();
     }
 
     public static void main(String[] args) {
