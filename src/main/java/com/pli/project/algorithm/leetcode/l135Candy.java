4a2648e24b161574fa0ff8ea759e4d1248eeed3d
allenlipeng47 <allen.lipeng47@gmail.com>
2016-07-20 23:41:58 -0700

    Added more comments.

--- src/main/java/com/pli/project/algorithm/leetcode/l135Candy.java
+++ src/main/java/com/pli/project/algorithm/leetcode/l135Candy.java
@@ -11,21 +11,21 @@ public class l135Candy {
         for (int i = 1; i < ratings.length; i++) {
             if (ratings[i] >= ratings[i - 1]) {
                 if (countDown > 0) {
-                    total += countDown * (countDown + 1) / 2;
-                    if (countDown >= pre) {
+                    total += countDown * (countDown + 1) / 2;   // progression part
+                    if (countDown >= pre) { // check if pre is tall enough
                         total += countDown - pre + 1;
                     }
-                    pre = 1;
+                    pre = 1;    // when ascending and there is countDown, prev should be 1
                     countDown = 0;
                 }
-                pre = ratings[i] == ratings[i - 1] ? 1 : pre + 1;
+                pre = ratings[i] == ratings[i - 1] ? 1 : pre + 1;   // when equals to previous one, set to 1. Else set to prev + 1
                 total += pre;
             }
             else {
                 countDown++;
             }
         }
-        if (countDown > 0) {
+        if (countDown > 0) {    // check if there is countDown in the end
             total += countDown * (countDown + 1) / 2;
             if (countDown >= pre) {
                 total += countDown - pre + 1;
