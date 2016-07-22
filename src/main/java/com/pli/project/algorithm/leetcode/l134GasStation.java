86caa665eace434afb322175f01e51e176b2016e
allenlipeng47 <allen.lipeng47@gmail.com>
2016-07-21 22:13:34 -0700

    Added more comment.

--- src/main/java/com/pli/project/algorithm/leetcode/l134GasStation.java
+++ src/main/java/com/pli/project/algorithm/leetcode/l134GasStation.java
@@ -7,6 +7,7 @@ package com.pli.project.algorithm.leetcode;
 public class l134GasStation {
 
     public int canCompleteCircuit(int[] gas, int[] cost) {
+        // test see if there is a result.
         int total = 0;
         for (int i = 0; i < gas.length; i++) {
             total += gas[i] - cost[i];
@@ -14,12 +15,13 @@ public class l134GasStation {
         if (total < 0) {
             return -1;
         }
-        int net = 0, ans = 0;
+        // calculate the aggregation until we find a position which the aggregation from it to the end is greater than or equal to 0
+        int aggr = 0, ans = 0;
         for (int i = 0; i < gas.length; i++) {
-            net += gas[i] - cost[i];
-            if (net < 0) {
+            aggr += gas[i] - cost[i];
+            if (aggr < 0) {
                 ans = i + 1;
-                net = 0;
+                aggr = 0;
             }
         }
         return ans;
