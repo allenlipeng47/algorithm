8008861f479da8e1dbd09e79586378c70bf51aa5
allenlipeng47 <allen.lipeng47@gmail.com>
2016-07-31 22:54:33 -0700

    https://leetcode.com/problems/same-tree/

---
+++ src/main/java/com/pli/project/algorithm/leetcode/l100SameTree.java
@@ -0,0 +1,19 @@
+package com.pli.project.algorithm.leetcode;
+
+/**
+ * Created by lipeng on 2016/7/27.
+ */
+public class l100SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+
+}
