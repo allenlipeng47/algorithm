b30e50dcf4da525f5da761365ee5fe3011350bc3
allenlipeng47 <allen.lipeng47@gmail.com>
2016-08-20 10:21:54 -0700

    Added simplified version.

--- src/main/java/com/pli/project/algorithm/amz/HouseRobber.java
+++ src/main/java/com/pli/project/algorithm/amz/HouseRobber.java
@@ -5,13 +5,23 @@ package com.pli.project.algorithm.amz;
  */
 public class HouseRobber {
 
-    public int rob(int[] nums) {
+    public int rob2(int[] nums) {
         int[] maxRob = new int[nums.length + 2], max = new int[nums.length + 2];
         for (int i = 0; i < nums.length; i++) {
             maxRob[i + 2] = nums[i] + max[i];
             max[i + 2] = Math.max(maxRob[i + 2], max[i + 1]);
         }
         return max[nums.length + 1];
+    }
+
+    public int rob(int[] nums) {
+        int rob = 0, noRob = 0;
+        for (int i : nums) {
+            int preRob = rob, preNoRob = noRob;
+            rob = i + preNoRob;
+            noRob = Math.max(preRob, preNoRob);
+        }
+        return Math.max(rob, noRob);
     }
 
 }
