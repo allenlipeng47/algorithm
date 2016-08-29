8475dc21e04e02f1f1a7c1060a5b87049a761ab7
allenlipeng47 <allen.lipeng47@gmail.com>
2016-08-29 07:41:17 -0700

    Added more comment

--- src/main/java/com/pli/project/algorithm/leetcode/GuessNumberII.java
+++ src/main/java/com/pli/project/algorithm/leetcode/GuessNumberII.java
@@ -4,6 +4,7 @@ package com.pli.project.algorithm.leetcode;
  * Created by lipeng on 2016/8/28.
  * https://leetcode.com/problems/guess-number-higher-or-lower-ii/
  * dp[i][j] = min (i<=k<=j) { k + max(dp[i][k-1], dp[k+1][j]) }
+ * The goal is to calculate dp[1][n]
  */
 public class GuessNumberII {
 
