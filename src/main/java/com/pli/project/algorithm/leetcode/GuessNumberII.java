ca29afc28d7f4bf90d31d6adc29a7dcac5187063
allenlipeng47 <allen.lipeng47@gmail.com>
2016-08-29 07:38:42 -0700

    https://leetcode.com/problems/guess-number-higher-or-lower-ii/ dp[i][j] = min (i<=k<=j) { k + max(dp[i][k-1], dp[k+1][j]) }

--- src/main/java/com/pli/project/algorithm/leetcode/GuessNumberII.java
+++ src/main/java/com/pli/project/algorithm/leetcode/GuessNumberII.java
@@ -2,22 +2,24 @@ package com.pli.project.algorithm.leetcode;
 
 /**
  * Created by lipeng on 2016/8/28.
+ * https://leetcode.com/problems/guess-number-higher-or-lower-ii/
+ * dp[i][j] = min (i<=k<=j) { k + max(dp[i][k-1], dp[k+1][j]) }
  */
 public class GuessNumberII {
 
     public static int getMoneyAmount(int n) {
         int[][] dp = new int[n + 1][n + 1];
         for (int delta = 1; delta < n + 1; delta++) {
-            for (int i = 0; i + delta < n + 1; i++) {
+            for (int i = 1; i + delta < n + 1; i++) {
                 int j = i + delta;
                 dp[i][j] = Integer.MAX_VALUE;
-                for (int k = 1; k <= j; k++) {
+                for (int k = i; k <= j; k++) {
                     dp[i][j] = Math.min(dp[i][j],
                             k + Math.max(i <= k - 1? dp[i][k - 1] : 0, k + 1 <= j ? dp[k + 1][j] : 0));
                 }
             }
         }
-        return dp[0][n];
+        return dp[1][n];
     }
 
     public static void main(String[] args) {
