d5ecd0bc054f0c4a33416803580fba61b3d80787
allenlipeng47 <allen.lipeng47@gmail.com>
2016-08-29 22:09:05 -0700

    Fixed sign bug.

--- src/main/java/com/pli/project/algorithm/leetcode/l43Multiply.java
+++ src/main/java/com/pli/project/algorithm/leetcode/l43Multiply.java
@@ -12,7 +12,7 @@ public class l43Multiply {
         int[] digit = new int[num1.length() + num2.length()];
         for (int i = num1.length() - 1; i >= 0; i--) {
             for (int j = num2.length() - 1; j >= 0; j--) {
-                int posHigh = i + j, posLow = i + j - 1;
+                int posHigh = i + j, posLow = i + j + 1;
                 int curr = (num1.charAt(i) - '0') * (num2.charAt(j) - '0') + digit[posLow];
                 digit[posLow]  = curr % 10;
                 digit[posHigh] += curr / 10;
