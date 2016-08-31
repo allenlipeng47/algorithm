96f60b45fd6e341131ccd3a1e44f7c9a9937a55b
allenlipeng47 <allen.lipeng47@gmail.com>
2016-08-30 23:54:30 -0700

    https://leetcode.com/problems/shuffle-an-array/

---
+++ src/main/java/com/pli/project/algorithm/leetcode/l384Shuffling.java
@@ -0,0 +1,40 @@
+package com.pli.project.algorithm.leetcode;
+
+import java.util.Random;
+
+/**
+ * Created by lipeng on 2016/8/30.
+ * https://leetcode.com/problems/shuffle-an-array/
+ */
+public class l384Shuffling {
+
+    int[] nums;
+    Random random;
+
+    public l384Shuffling(int[] nums) {
+        random = new Random();
+        this.nums = nums;
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return nums;
+    }
+
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        int[] ans = nums.clone();
+        for (int i = 0; i < ans.length; i++) {
+            int j = random.nextInt(i + 1);
+            swap(ans, i, j);
+        }
+        return ans;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+}
