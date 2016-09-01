e8228b207663bf182c4cdc92b303c15cd9fca509
allenlipeng47 <allen.lipeng47@gmail.com>
2016-08-31 22:39:32 -0700

    https://leetcode.com/problems/first-unique-character-in-a-string/ http://www.geeksforgeeks.org/given-a-string-find-its-first-non-repeating-character/

---
+++ src/main/java/com/pli/project/algorithm/leetcode/l387FirstUniqueCharInString.java
@@ -0,0 +1,29 @@
+package com.pli.project.algorithm.leetcode;
+
+/**
+ * Created by lipeng on 2016/8/31.
+ * https://leetcode.com/problems/first-unique-character-in-a-string/
+ * http://www.geeksforgeeks.org/given-a-string-find-its-first-non-repeating-character/
+ * During the first time scan,
+ * 1. Count the frequency of each char.
+ * 2. Store the position of each char where it is firstly visited
+ * In the second time, just scan the 26 times to find the lowest position iwhere the count is 1.
+ */
+public class l387FirstUniqueCharInString {
+
+    public int firstUniqChar(String s) {
+        int[] firstTime = new int[26], count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (++count[ch - 'a'] == 1) {
+                firstTime[ch - 'a'] = i;
+            }
+        }
+        int ans = -1;
+        for (int i = 0; i < firstTime.length; i++) {
+            ans = (count[i] == 1 && firstTime[i] < ans) ? firstTime[i] : ans;
+        }
+        return ans;
+    }
+
+}
